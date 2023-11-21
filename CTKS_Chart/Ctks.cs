dd760a2fa83be623695b43c00711614fe93c71aa
Roman Pecho <f>
2023-11-21 21:08:38 +0100

    Add project files.

---
+++ CTKS_Chart/Ctks.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace CTKS_Chart
+{
+  public enum LineType
+  {
+    LeftBottom,
+    RightBttom,
+    LeftTop,
+    RightTop
+  }
+
+  public class Ctks
+  {
+    private Canvas canvas;
+    public Ctks(Canvas canvas)
+    {
+      this.canvas = canvas;
+    }
+
+
+    public void CreateLine(int candleIndex, int secondIndex, LineType lineType, Func<double, double> getCanvasValue)
+    {
+      var candles = canvas.Children.OfType<Rectangle>().ToList();
+      var firstRect = candles[candleIndex];
+      var secondrect = candles[secondIndex];
+
+      var bottom1 = Canvas.GetBottom(firstRect);
+      var bottom2 = Canvas.GetBottom(secondrect);
+
+      PathFigure myPathFigure = new PathFigure();
+      LineSegment myLineSegment = new LineSegment();
+      Path myPath = new Path();
+
+      myPath.Stroke = Brushes.Yellow;
+      myPath.StrokeThickness = 1;
+
+      double y1 = 0.0;
+      double y2 = 0.0;
+
+      double x1 = 0.0;
+      double x2 = 0.0;
+
+      if (lineType == LineType.RightBttom)
+      {
+        myPathFigure.StartPoint = new Point(Canvas.GetLeft(firstRect) + firstRect.Width, canvas.ActualHeight - bottom1);
+        myLineSegment.Point = new Point(Canvas.GetLeft(secondrect) + secondrect.Width, canvas.ActualHeight - bottom2);
+
+        y1 = bottom1;
+        y2 = bottom2;
+
+        x1 = Canvas.GetLeft(firstRect) + firstRect.Width;
+        x2 = Canvas.GetLeft(secondrect) + secondrect.Width;
+
+        Canvas.SetBottom(myPath, y1);
+      }
+      else if (lineType == LineType.LeftTop)
+      {
+        myPathFigure.StartPoint = new Point(Canvas.GetLeft(firstRect), canvas.ActualHeight - bottom1 - firstRect.Height);
+        myLineSegment.Point = new Point(Canvas.GetLeft(secondrect), canvas.ActualHeight - bottom2 - secondrect.Height);
+
+        y1 = bottom1 + firstRect.Height;
+        y2 = bottom2 + secondrect.Height;
+
+        x1 = Canvas.GetLeft(firstRect);
+        x2 = Canvas.GetLeft(secondrect);
+
+        Canvas.SetBottom(myPath, y1);
+      }
+      else if (lineType == LineType.RightTop)
+      {
+        myPathFigure.StartPoint = new Point(Canvas.GetLeft(firstRect) + firstRect.Width, canvas.ActualHeight - bottom1 - firstRect.Height);
+        myLineSegment.Point = new Point(Canvas.GetLeft(secondrect) + secondrect.Width, canvas.ActualHeight - bottom2 - secondrect.Height);
+
+        y1 = bottom1 + firstRect.Height;
+        y2 = bottom2 + secondrect.Height;
+
+        x1 = Canvas.GetLeft(firstRect) + firstRect.Width;
+        x2 = Canvas.GetLeft(secondrect) + secondrect.Width;
+      }
+
+      var x3 = canvas.ActualWidth * 1.5;
+      var y3 = GetPointOnLine(x1, y1, x2, y2, x3);
+
+      LineSegment lastSegment = new LineSegment();
+      lastSegment.Point = new Point(x3, canvas.ActualHeight - y3);
+
+      if (lineType == LineType.RightTop)
+        Canvas.SetBottom(myPath, canvas.ActualHeight - lastSegment.Point.Y);
+
+
+      AddIntersections(x1, y1, x2, y2, getCanvasValue);
+
+      PathSegmentCollection myPathSegmentCollection = new PathSegmentCollection();
+      myPathSegmentCollection.Add(myLineSegment);
+      myPathSegmentCollection.Add(lastSegment);
+
+      myPathFigure.Segments = myPathSegmentCollection;
+
+      PathFigureCollection myPathFigureCollection = new PathFigureCollection();
+      myPathFigureCollection.Add(myPathFigure);
+
+      PathGeometry myPathGeometry = new PathGeometry();
+      myPathGeometry.Figures = myPathFigureCollection;
+      myPath.Data = myPathGeometry;
+
+
+      canvas.Children.Add(myPath);
+
+    }
+
+    private double GetPointOnLine(double x1, double y1, double x2, double y2, double x3)
+    {
+      double deltaY = y2 - y1;
+      double deltaX = x2 - x1;
+
+      var slope = deltaY / deltaX;
+      //https://www.mathsisfun.com/algebra/line-equation-point-slope.html
+      //y − y1 = m(x − x1)
+      //x = x1 + ((y -y1) / m)
+      //y = m(x − x1) + y1
+      return (slope * (x3 - x1)) + y1;
+    }
+
+    private void AddIntersections(double x1, double y1, double x2, double y2, Func<double, double> getCanvasValue)
+    {
+      var candles = canvas.Children.OfType<Rectangle>().ToList();
+
+      var circle = new Ellipse();
+      var size = 5;
+      circle.Width = size;
+      circle.Height = size;
+      circle.Fill = Brushes.Red;
+
+      var lastCandl = candles[candles.Count - 1];
+      var actualLeft = Canvas.GetLeft(lastCandl) + lastCandl.Width / 2;
+      var actual = GetPointOnLine(x1, y1, x2, y2, actualLeft);
+
+      Canvas.SetLeft(circle, actualLeft - size / 2.0);
+      Canvas.SetBottom(circle, actual - size / 2.0);
+
+
+      var line = new Line();
+      line.Stroke = Brushes.Gray;
+      line.StrokeThickness = 5;
+      line.X1 = 150;
+      line.X2 = canvas.ActualWidth;
+      line.StrokeDashArray = new DoubleCollection() { 1, 1 };
+
+      
+
+      var lineY = canvas.ActualHeight - actual;
+
+      line.Y1 = lineY;
+      line.Y2 = lineY;
+
+      var text = new TextBlock();
+      text.Text = getCanvasValue(actual).ToString("N2") + "";
+      text.Foreground = Brushes.White;
+      Panel.SetZIndex(text, 99);
+
+      Canvas.SetLeft(text, 0);
+      Canvas.SetBottom(text, actual);
+
+      canvas.Children.Add(circle);
+      canvas.Children.Add(line);
+      canvas.Children.Add(text);
+    }
+  }
+}
---
+++ CTKS_Chart/MainWindow.xaml.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Path = System.Windows.Shapes.Path;
+
+namespace CTKS_Chart
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public ObservableCollection<Candle> ChartPoints { get; set; } = new ObservableCollection<Candle>();
+
+    public ObservableCollection<KeyValuePair<DateTime, double>> ForexData { get; set; } = new ObservableCollection<KeyValuePair<DateTime, double>>();
+
+    public double CanvasHeight
+    {
+      get
+      {
+        return main_canvas.ActualHeight * 0.75;
+      }
+    }
+
+    private Ctks ctks;
+    public MainWindow()
+    {
+      InitializeComponent();
+      DataContext = this;
+
+      ctks = new Ctks(main_canvas);
+
+    }
+
+    double maxValue = 600;
+    double minValue = 25;
+
+    private async void ForexChart_Loaded()
+    {
+      //var tradingView_12m = "D:\\Aplikacie\\Skusobne\\CTKS_Chart\\CTKS_Chart\\BTC-USD.csv";
+
+      var localtion = "D:\\Aplikacie\\Skusobne\\CTKS_Chart\\CTKS_Chart";
+      var tradingView_12m = $"{localtion}\\INDEX BTCUSD, 12M.csv";
+      var tradingView_spy_12 = $"{localtion}\\BATS SPY, 12M.csv";
+
+      var file = File.ReadAllText(tradingView_spy_12);
+
+      var lines = file.Split("\n");
+      CultureInfo.CurrentCulture = new CultureInfo("en-US");
+
+      foreach (var line in lines.Skip(1))
+      {
+        var data = line.Split(",");
+
+        double.TryParse(data[0], out var timeParsed);
+        double.TryParse(data[1], out var openParsed);
+        double.TryParse(data[2], out var highParsed);
+        double.TryParse(data[3], out var lowParsed);
+        double.TryParse(data[4], out var closeParsed);
+
+
+        ChartPoints.Add(new Candle()
+        {
+          Close = closeParsed,
+          Open = openParsed,
+          High = highParsed,
+          Low = lowParsed
+        });
+      }
+
+      ChartPoints = new ObservableCollection<Candle>(ChartPoints.Take(ChartPoints.Count - 0));
+
+      CreateChart();
+    }
+
+    private double GetCanvasValue(double value)
+    {
+      var logValue = Math.Log10(value);
+      var logMaxValue = Math.Log10(maxValue);
+      var logMinValue = Math.Log10(minValue);
+
+      var logRange = logMaxValue - logMinValue;
+      double diffrence = logValue - logMinValue;
+
+      return diffrence * CanvasHeight / logRange;
+    }
+
+    private void CreateChart()
+    {
+      var canvasWidth = main_canvas.ActualWidth * 0.75;
+
+      for (int i = 0; i < ChartPoints.Count; i++)
+      {
+        var point = ChartPoints[i];
+        var valueForCanvas = GetCanvasValue(point.Close);
+        var width = canvasWidth / ChartPoints.Count;
+
+
+        var green = i > 0 ? ChartPoints[i - 1].Close < point.Close : point.Open < point.Close;
+
+        Rectangle rec = new Rectangle()
+        {
+          Width = width,
+          Height = 25,
+          Fill = green ? Brushes.Green : Brushes.Red,
+        };
+
+        Panel.SetZIndex(rec, 99);
+
+
+        var open = i > 0 ? GetCanvasValue(ChartPoints[i - 1].Close) : GetCanvasValue(ChartPoints[i].Open);
+
+        if (green)
+        {
+          rec.Height = valueForCanvas - open;
+        }
+        else
+        {
+          rec.Height = open - valueForCanvas;
+        }
+
+        if (rec.Height < 0.5)
+        {
+          rec.Height = 0.5;
+        }
+
+        main_canvas.Children.Add(rec);
+
+        if (green)
+          Canvas.SetBottom(rec, open);
+        else
+          Canvas.SetBottom(rec, open - rec.Height);
+
+        Canvas.SetLeft(rec, ((i + 1) * width) + 2);
+
+      }
+
+      var candles = main_canvas.Children.OfType<Rectangle>().ToList();
+
+      for (int i = 0; i < ChartPoints.Count - 2; i++)
+      {
+        var currentCandle = ChartPoints[i];
+        var nextCandle = ChartPoints[i + 1];
+
+        //await Task.Delay(1000);
+
+        if (currentCandle.IsGreen)
+        {
+          if (nextCandle.IsGreen)
+            ctks.CreateLine(i, i + 1, LineType.LeftTop, GetValueFromCanvas);
+
+          if (currentCandle.Close < nextCandle.Close || (currentCandle.Open < nextCandle.Close))
+            ctks.CreateLine(i, i + 1, LineType.RightBttom, GetValueFromCanvas);
+        }
+      }
+    }
+
+    private double GetValueFromCanvas(double value)
+    {
+      var logMaxValue = Math.Log10(maxValue);
+      var logMinValue = Math.Log10(minValue);
+
+      var logRange = logMaxValue - logMinValue;
+
+      return Math.Pow(10, (value * logRange / CanvasHeight) + logMinValue);
+    }
+
+    private void main_canvas_Loaded(object sender, RoutedEventArgs e)
+    {
+      ForexChart_Loaded();
+    }
+
+    private void Canvas_MouseMove(object sender, MouseEventArgs e)
+    {
+      var mosue = Mouse.GetPosition(main_canvas);
+
+      Canvas.SetLeft(coordinates, mosue.X + 20);
+      Canvas.SetTop(coordinates, mosue.Y - 10);
+
+      var canvasValue = mosue.Y;
+
+      coordinates.Text = $"{GetValueFromCanvas(main_canvas.ActualHeight - canvasValue).ToString("N2")}";}
+
+    private void main_canvas_SizeChanged(object sender, SizeChangedEventArgs e)
+    {
+
+      if (e.WidthChanged)
+      {
+        main_canvas.Children.Clear();
+        main_canvas.Children.Add(coordinates);
+
+        CreateChart();
+      }
+     
+    }
+  }
+  }
+
+  public class Candle
+  {
+    public double Close { get; set; }
+    public double Open { get; set; }
+    public double High { get; set; }
+    public double Low { get; set; }
+
+    public bool IsGreen
+    {
+      get { return Close > Open; }
+    }
+  }
+
